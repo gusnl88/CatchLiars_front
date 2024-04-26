5f6590458b8ae7c90140b23f68a2581a4bdd421e
ohjin5 <ok024097@gmail.com>
2024-04-26 16:08:55 +0900

    feat: 로그인 css 작업 및 회원가입 중복검사 기능 완료

--- src/components/user/LoginForm.jsx
+++ src/components/user/LoginForm.jsx
@@ -1,9 +1,117 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { useDispatch } from "react-redux";
 import { login } from "../../store/modules/login";
 import RegisterForm from "./RegisterForm"; // 회원가입 폼 컴포넌트 임포트
 import { useNavigate } from "react-router-dom";
+import axiosUtils from "../../utils/axiosUtils";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center; // 센터 정렬을 원한다면 추가
+    justify-content: center; // 세로 방향으로 가운데 정렬을 원한다면 추가
+    width: 100%; // 전체 너비 사용
+    min-height: 100vh; // 뷰포트의 전체 높이 사용
+`;
+
+const Container1 = styled.div`
+    width: 1000px;
+    margin: 0 auto 20px;
+    padding: 20px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+`;
+const Container2 = styled.div`
+    width: 1000px;
+    margin: 0 auto 20px;
+    padding: 20px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    /* justify-content: center; */
+    align-items: center;
+`;
+
+const SubTitle = styled.h2`
+    font-size: 1.5em;
+    color: #333;
+    text-align: center; // 중앙 정렬
+    margin-top: 20px;
+    margin-bottom: 10px;
+`;
+
+const Text = styled.div`
+    font-size: 1em;
+    color: #666;
+    line-height: 1.5;
+    text-align: justify; // 양쪽 정렬
+    margin: 0 20px; // 좌우 여백
+`;
+const IdContainer = styled.div`
+    display: flex; // 이 줄을 추가하여 flexbox 레이아웃을 활용
+    align-items: center; // 세로 중앙 정렬
+    margin-bottom: 10px; // 요소 간 간격 추가
+`;
+
+const DivId = styled.div`
+    width: 50%;
+    padding: 8px;
+    display: flex; // DivId 내부 요소도 flex로 정렬
+    justify-content: flex-start; // 라벨을 오른쪽 정렬
+`;
+
+const Inputid = styled.input`
+    width: 100%;
+    padding: 8px;
+    margin-left: 10px; // 라벨과 입력 필드 사이의 간격
+`;
+
+const PwContainer = styled.div`
+    display: flex; // 이 줄을 추가하여 flexbox 레이아웃을 활용
+    align-items: center; // 세로 중앙 정렬
+    margin-bottom: 10px; // 요소 간 간격 추가
+`;
+
+const DivPw = styled.div`
+    width: 50%;
+    padding: 8px;
+    display: flex; // DivId 내부 요소도 flex로 정렬
+    justify-content: flex-start; // 라벨을 오른쪽 정렬
+`;
+
+const Inputpw = styled.input`
+    width: 100%;
+    padding: 8px;
+    margin-left: 10px; // 라벨과 입력 필드 사이의 간격
+`;
+const Title = styled.h1`
+    text-align: center;
+    padding-top: 10px;
+    padding-right: 20px;
+    padding-bottom: 50px;
+    padding-left: 40px;
+`;
+
+const CheckButton = styled.button`
+    padding: 8px 10px; // Inputid와 동일한 패딩을 적용
+    /* margin-top: 5px; */
+    margin-left: 5px;
+    cursor: pointer; // 버튼에 마우스 오버시 커서 변경
+    background-color: #007bff; // 배경 색상 설정
+    color: white; // 텍스트 색상 설정
+    border: none;
+    border-radius: 4px; // 테두리 둥글게 처리
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center; // 가로축 중앙 정렬
+    align-items: center; // 세로축 중앙 정렬
+    gap: 10px; // 버튼 사이의 간격
+    margin-top: 20px; // 상단 여백
+`;
 
 function LoginForm() {
     const [formData, setFormData] = useState({
@@ -22,10 +130,10 @@ function LoginForm() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post(
-                `${process.env.REACT_APP_API_SERVER}/users/signin`,
-                formData,
-                { withCredentials: true }
+            const response = await axiosUtils.post(
+                `/users/signin`,
+                formData
+                // { withCredentials: true }
             );
             if (response.data) {
                 setShowModal(true);
@@ -56,51 +164,72 @@ function LoginForm() {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <h2>로그인</h2>
-                <div>
-                    <label>아이디:</label>
-                    <input
-                        type="text"
-                        name="inputId"
-                        value={formData.inputId}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>비밀번호:</label>
-                    <input
-                        type="password"
-                        name="inputPw"
-                        value={formData.inputPw}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <button type="submit">로그인</button>
-                <button type="button" onClick={handleSignup}>
-                    회원가입
-                </button>
-            </form>
-            {showModal && (
-                <div
-                    style={{
-                        position: "fixed",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        backgroundColor: "white",
-                        padding: "20px",
-                        zIndex: 1000,
-                    }}
-                >
-                    <p>로그인 성공!</p>
-                    <button onClick={closeModalAndNavigate}>확인</button>
-                </div>
-            )}
-        </div>
+        <MainContainer>
+            <Container1>
+                <Title>로그인을 위한 절차</Title>
+                <SubTitle>로그인 주의사항</SubTitle>
+                <Text>
+                    <div>로그인 시 다음 사항을 유의해 주세요:</div>
+                    <ul>
+                        <li>모든 입력 필드는 반드시 작성해야 합니다.</li>
+                        <li>아이디와 비밀번호는 5글자 이상 입력해주세요.</li>
+                        <li>처음 이용하시는 분은 아래 회원가입을 통해 진행해주세요.</li>
+                    </ul>
+                </Text>
+            </Container1>
+            <Container2>
+                <form onSubmit={handleSubmit}>
+                    <Title>로그인</Title>
+                    <IdContainer>
+                        <DivId>
+                            <label>아이디:</label>
+                        </DivId>
+                        <Inputid
+                            type="text"
+                            name="inputId"
+                            value={formData.inputId}
+                            onChange={handleChange}
+                            required
+                        />
+                    </IdContainer>
+                    <PwContainer>
+                        <DivPw>
+                            <label>비밀번호:</label>
+                        </DivPw>
+                        <Inputpw
+                            type="password"
+                            name="inputPw"
+                            value={formData.inputPw}
+                            onChange={handleChange}
+                            required
+                        />
+                    </PwContainer>
+                    <ButtonContainer>
+                        <CheckButton type="submit">로그인</CheckButton>
+                        <CheckButton type="button" onClick={handleSignup}>
+                            회원가입
+                        </CheckButton>
+                    </ButtonContainer>
+                </form>
+
+                {showModal && (
+                    <div
+                        style={{
+                            position: "fixed",
+                            top: "50%",
+                            left: "50%",
+                            transform: "translate(-50%, -50%)",
+                            backgroundColor: "white",
+                            padding: "20px",
+                            zIndex: 1000,
+                        }}
+                    >
+                        <p>로그인 성공!</p>
+                        <button onClick={closeModalAndNavigate}>확인</button>
+                    </div>
+                )}
+            </Container2>
+        </MainContainer>
     );
 }
 
--- src/components/user/RegisterForm.jsx
+++ src/components/user/RegisterForm.jsx
@@ -1,5 +1,150 @@
 import React, { useState } from "react";
+import LoginForm from "./LoginForm";
+import axiosUtils from "../../utils/axiosUtils";
 import axios from "axios";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center; // 센터 정렬을 원한다면 추가
+    justify-content: center; // 세로 방향으로 가운데 정렬을 원한다면 추가
+    width: 100%; // 전체 너비 사용
+    min-height: 100vh; // 뷰포트의 전체 높이 사용
+`;
+
+const Container = styled.div`
+    width: 1000px;
+    margin: 0 auto 20px;
+    padding: 20px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+`;
+
+const SubTitle = styled.h2`
+    font-size: 1.5em;
+    color: #333;
+    text-align: center; // 중앙 정렬
+    margin-top: 20px;
+    margin-bottom: 10px;
+`;
+
+const Text = styled.div`
+    font-size: 1em;
+    color: #666;
+    line-height: 1.5;
+    text-align: justify; // 양쪽 정렬
+    margin: 0 20px; // 좌우 여백
+`;
+const IdContainer = styled.div`
+    display: flex; // 이 줄을 추가하여 flexbox 레이아웃을 활용
+    align-items: center; // 세로 중앙 정렬
+    margin-bottom: 10px; // 요소 간 간격 추가
+`;
+
+const DivId = styled.div`
+    width: 10%;
+    padding: 8px;
+    display: flex; // DivId 내부 요소도 flex로 정렬
+    justify-content: flex-end; // 라벨을 오른쪽 정렬
+`;
+
+const Inputid = styled.input`
+    width: 50%;
+    padding: 8px;
+    margin-left: 10px; // 라벨과 입력 필드 사이의 간격
+`;
+
+const CheckButton = styled.button`
+    padding: 8px 10px; // Inputid와 동일한 패딩을 적용
+    /* margin-top: 5px; */
+    margin-left: 5px;
+    cursor: pointer; // 버튼에 마우스 오버시 커서 변경
+    background-color: #007bff; // 배경 색상 설정
+    color: white; // 텍스트 색상 설정
+    border: none;
+    border-radius: 4px; // 테두리 둥글게 처리
+`;
+const PwContainer = styled.div`
+    display: flex; // 이 줄을 추가하여 flexbox 레이아웃을 활용
+    align-items: center; // 세로 중앙 정렬
+    margin-bottom: 10px; // 요소 간 간격 추가
+`;
+
+const DivPw = styled.div`
+    width: 10%;
+    padding: 8px;
+    display: flex; // DivId 내부 요소도 flex로 정렬
+    justify-content: flex-end; // 라벨을 오른쪽 정렬
+`;
+
+const Inputpw = styled.input`
+    width: 50%;
+    padding: 8px;
+    margin-left: 10px; // 라벨과 입력 필드 사이의 간격
+`;
+const Title = styled.h1`
+    text-align: center;
+    padding-top: 10px;
+    padding-right: 20px;
+    padding-bottom: 50px;
+    padding-left: 40px;
+`;
+
+const NmContainer = styled.div`
+    display: flex; // 이 줄을 추가하여 flexbox 레이아웃을 활용
+    align-items: center; // 세로 중앙 정렬
+    margin-bottom: 10px; // 요소 간 간격 추가
+`;
+
+const DivNm = styled.div`
+    width: 10%;
+    padding: 8px;
+    display: flex; // DivId 내부 요소도 flex로 정렬
+    justify-content: flex-end; // 라벨을 오른쪽 정렬
+`;
+
+const Inputnm = styled.input`
+    width: 50%;
+    padding: 8px;
+    margin-left: 10px; // 라벨과 입력 필드 사이의 간격
+`;
+
+const EmContainer = styled.div`
+    display: flex; // 이 줄을 추가하여 flexbox 레이아웃을 활용
+    align-items: center; // 세로 중앙 정렬
+    margin-bottom: 10px; // 요소 간 간격 추가
+`;
+
+const DivEm = styled.div`
+    width: 10%;
+    padding: 8px;
+    display: flex; // DivId 내부 요소도 flex로 정렬
+    justify-content: flex-end; // 라벨을 오른쪽 정렬
+`;
+const Inputem = styled.input`
+    width: 50%;
+    padding: 8px;
+    margin-left: 10px;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center; // 가로축 중앙 정렬
+    align-items: center; // 세로축 중앙 정렬
+    gap: 10px; // 버튼 사이의 간격
+    margin-top: 20px; // 상단 여백
+`;
+
+const Button = styled.button`
+    padding: 10px 20px;
+    margin-top: 5px;
+`;
+
+const ErrorSpan = styled.span`
+    color: red;
+`;
 
 function RegisterForm() {
     const [formData, setFormData] = useState({
@@ -10,118 +155,169 @@ function RegisterForm() {
     });
     const [errors, setErrors] = useState({});
     const [serverError, setServerError] = useState("");
+    const [showRegister, setShowRegister] = useState(true);
+    const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태
+    const [availabilityMessages, setAvailabilityMessages] = useState({});
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
-        // Clear errors as the user types
-        setErrors({ ...errors, [e.target.name]: "" });
+        // setErrors({ ...errors, [e.target.name]: "" });
     };
 
-    // Function to check duplicate id or email
-    const checkDuplicate = async (field, value) => {
-        if (!value) return; // If the field is empty, no need to check
+    const handleBack = () => {
+        setShowRegister(false); // 뒤로 가기 버튼 클릭시 LoginForm 표시
+    };
+
+    const checkDuplicate = async (field) => {
+        if (!formData[field]) {
+            setErrors({ ...errors, [field]: "This field cannot be empty." });
+            setAvailabilityMessages({ ...availabilityMessages, [field]: "" }); // 메시지 초기화
+            return;
+        }
         try {
-            const response = await axios.post(
-                `${process.env.REACT_APP_API_SERVER}/users/checkDuplicate`,
-                { field, value }
-            );
+            const response = await axiosUtils.post(`/users/check-duplicate`, {
+                field: field,
+                value: formData[field],
+            });
             if (response.data === false) {
-                // Assuming the server returns false for duplicates
-                setErrors((prevErrors) => ({
-                    ...prevErrors,
-                    [field]: `This ${field} is already taken.`,
-                }));
+                setErrors({ ...errors, [field]: "이미 사용중 입니다." });
+                setAvailabilityMessages({ ...availabilityMessages, [field]: "이미 사용중입니다." }); // 메시지 초기화
+            } else {
+                setErrors({ ...errors, [field]: "" }); // 에러 메시지 초기화
+                setAvailabilityMessages({ ...availabilityMessages, [field]: "사용 가능합니다." }); // 성공 메시지 설정
             }
         } catch (error) {
-            console.error("Error checking duplicate:", error);
-            setServerError("Error checking for duplicates. Please try again later.");
+            console.error("Failed to check duplicate:", error);
+            setErrors({ ...errors, [field]: "Failed to check. Please try again." });
+            setAvailabilityMessages({ ...availabilityMessages, [field]: "" }); // 메시지 초기화
         }
     };
+
+    if (!showRegister) {
+        return <LoginForm />; // showRegister가 false일 때 LoginForm을 렌더링
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         // Check for any existing errors or duplicates before submitting
         if (Object.values(errors).some((error) => error)) {
+            console.log(Object.values(errors));
+            console.log(Object.values(errors).some((error) => error));
             alert("Please resolve the errors before submitting.");
             return;
         }
 
         try {
-            const response = await axios.post(
-                `${process.env.REACT_APP_API_SERVER}/users/signup`,
+            const response = await axiosUtils.post(
+                // `${process.env.REACT_APP_API_SERVER}/users/signup`,
+                `/users/signup`,
                 formData
             );
             if (response.data === true) {
-                alert("Registration successful.");
+                alert("회원가입이 성공적으로 완료 되었습니다.");
             } else if (response.data.errors) {
                 setErrors(response.data.errors);
             } else {
-                setServerError("Unexpected server response. Please try again.");
+                setServerError("오류가 발생했습니다. 다시 실행해주세요.");
             }
         } catch (error) {
             console.error("Registration error:", error);
-            setServerError("Server error occurred during registration.");
+            setServerError("서버 에러 발생");
         }
     };
-
     return (
-        <div>
-            <h1>User Signup</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="id">ID:</label>
-                    <input
-                        type="text"
-                        id="id"
-                        name="id"
-                        value={formData.id}
-                        onChange={handleChange}
-                        onBlur={() => checkDuplicate("id", formData.id)}
-                        required
-                    />
-                    {errors.id && <span>{errors.id}</span>}
-                </div>
-                <div>
-                    <label htmlFor="pw">Password:</label>
-                    <input
-                        type="password"
-                        id="pw"
-                        name="pw"
-                        value={formData.pw}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="nickname">Nickname:</label>
-                    <input
-                        type="text"
-                        id="nickname"
-                        name="nickname"
-                        value={formData.nickname}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        onBlur={() => checkDuplicate("email", formData.email)}
-                        required
-                    />
-                    {errors.email && <span>{errors.email}</span>}
-                </div>
-                <div>
-                    <button type="submit">Sign Up</button>
-                </div>
-            </form>
-            {serverError && <p>{serverError}</p>}
-        </div>
+        <MainContainer>
+            <Container>
+                <Title>회원가입을 위한 절차</Title>
+                <SubTitle>회원가입 주의사항</SubTitle>
+                <Text>
+                    <div>회원가입 시 다음 사항을 유의해 주세요:</div>
+                    <ul>
+                        <li>모든 입력 필드는 반드시 작성해야 합니다.</li>
+                        <li>이메일 주소는 회원가입 확인 및 비밀번호 재설정에 사용됩니다.</li>
+                        <li>닉네임은 다른 사용자와 중복될 수 없습니다.</li>
+                        <li>아이디와 비밀번호는 5글자 이상 입력해주세요.</li>
+                        <li>닉네임은 3글자 이상 입력해주세요.</li>
+                        <li>이메일은 형식을 지켜서 작성해주시기 바립니다.</li>
+                    </ul>
+                </Text>
+            </Container>
+            <Container>
+                <Title>회원가입</Title>
+                <form onSubmit={handleSubmit}>
+                    <IdContainer>
+                        <DivId>
+                            <label htmlFor="id">ID:</label>
+                        </DivId>
+                        <Inputid
+                            type="text"
+                            id="id"
+                            name="id"
+                            value={formData.id}
+                            onChange={handleChange}
+                            required
+                        />
+                        <CheckButton type="button" onClick={() => checkDuplicate("id")}>
+                            Check ID
+                        </CheckButton>
+                        <span>{availabilityMessages.id}</span>
+                    </IdContainer>
+                    <PwContainer>
+                        <DivPw>
+                            <label htmlFor="pw">Password:</label>
+                        </DivPw>
+                        <Inputpw
+                            type="password"
+                            id="pw"
+                            name="pw"
+                            value={formData.pw}
+                            onChange={handleChange}
+                            required
+                        />
+                    </PwContainer>
+                    <NmContainer>
+                        <DivNm>
+                            <label htmlFor="nickname">Nickname:</label>
+                        </DivNm>
+                        <Inputnm
+                            type="text"
+                            id="nickname"
+                            name="nickname"
+                            value={formData.nickname}
+                            onChange={handleChange}
+                            required
+                        />
+                        <CheckButton type="button" onClick={() => checkDuplicate("nickname")}>
+                            Check nickname
+                        </CheckButton>
+                        <span>{availabilityMessages.nickname}</span>
+                    </NmContainer>
+                    <EmContainer>
+                        <DivEm>
+                            <label htmlFor="email">Email:</label>
+                        </DivEm>
+                        <Inputem
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+                        <CheckButton type="button" onClick={() => checkDuplicate("email")}>
+                            Check Email
+                        </CheckButton>
+                        <span>{availabilityMessages.email}</span>
+                    </EmContainer>
+                    <ButtonContainer>
+                        <CheckButton type="submit">Sign Up</CheckButton>
+                        <CheckButton type="button" onClick={handleBack}>
+                            돌아가기
+                        </CheckButton>
+                    </ButtonContainer>
+                </form>
+            </Container>
+        </MainContainer>
     );
 }
 
