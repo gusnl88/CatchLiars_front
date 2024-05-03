b7af315c6bbcd660d15a09b17ba11878dc89d56f
gusnl88 <gusnl88@naver.com>
2024-05-04 02:04:27 +0900

    fix:chatting page update fix

--- src/components/friend/ChattingRoom.jsx
+++ src/components/friend/ChattingRoom.jsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 import { io } from "socket.io-client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react"; // useRef 추가
 import { useSelector } from "react-redux";
 
 const Maincontainer = styled.div`
-    width: 300px;
     width: 300px;
     height: 600px;
     position: absolute;
@@ -15,12 +14,25 @@ const Maincontainer = styled.div`
     .chatting_container {
         width: 95%;
         height: 95%;
-        background: #0000ff57;
+        background: #9298a6f5;
         border-radius: 10px;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
         .chatting_main {
+            overflow-y: auto; /* 스크롤이 필요할 때만 나타나도록 수정 */
+            padding: 10px;
+            max-height: calc(100% - 100px);
+            &::-webkit-scrollbar {
+                width: 8px;
+            }
+            &::-webkit-scrollbar-thumb {
+                background-color: #888;
+                border-radius: 4px;
+            }
+            &::-webkit-scrollbar-track {
+                background-color: #f1f1f1;
+            }
         }
         .chatting_input {
             display: flex;
@@ -29,28 +41,58 @@ const Maincontainer = styled.div`
                 margin: 10px auto;
             }
         }
-        .me {
-            text-align: end;
-            margin: 5px auto;
+        .message-container {
+            display: flex;
+            justify-content: flex-end;
+            &.received {
+                justify-content: flex-start;
+            }
+
+            .read {
+                color: blue;
+                font-size: 6px;
+                margin: 5px; /* 읽음 표시의 위치 조정 */
+                align-items: end;
+                display: flex;
+            }
+            .message {
+                display: flex;
+                flex-direction: column;
+
+                background-color: yellow;
+                color: black;
+                border-radius: 10px;
+                padding: 5px 10px;
+                margin: 5px;
+
+                .time {
+                    font-size: 8px;
+                    margin: 5px;
+                }
+                word-break: break-word; /* 긴 단어가 있을 경우 줄바꿈 처리 */
+            }
         }
-        .other {
-            text-align: start;
-            margin: 5px auto;
+        .received .message {
+            background-color: white;
         }
         .notice {
             text-align: center;
             background-color: #8080807a;
             margin: 10px;
             border-radius: 4px;
+            .message {
+                background-color: #8080807a;
+            }
         }
     }
 `;
-export default function ChattingRoom({ roomId }) {
+
+export default function ChattingRoom({ roomId, setShowChattingRoom }) {
     const [socket, setSocket] = useState(null);
-    const [messages, setMessages] = useState([]);
     const [inputdata, setInputData] = useState("");
     const [msgList, setMsgList] = useState([]);
     const loginUser = useSelector((state) => state.loginReducer.user);
+    const chatContainerRef = useRef(null); // Ref 추가
 
     useEffect(() => {
         console.log(roomId);
@@ -64,17 +106,22 @@ export default function ChattingRoom({ roomId }) {
             if (data.sendUser === undefined) {
                 console.log("진입");
                 newMessage = {
-                    message: data.message,
+                    content: data.message,
                     type: "notice",
+                    is_read: true, // 공지사항은 읽음 상태로 표시
+                    // create_at: new Date(),
                 };
             } else {
                 const type = data.sendUser === loginUser.id ? "me" : "other";
                 newMessage = {
-                    message: data.message,
+                    content: data.message,
                     type: type,
+                    is_read: data.is_read,
+                    create_at: data.create_at,
+                    User: { id: data.sendUser },
                 };
             }
-            setMessages((prevMessages) => [...prevMessages, newMessage]);
+            setMsgList((prevMessages) => [...prevMessages, newMessage]);
         });
 
         socket.on("msgList", (data) => {
@@ -90,6 +137,12 @@ export default function ChattingRoom({ roomId }) {
         };
     }, [roomId]);
 
+    useEffect(() => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
+    }, [msgList]);
+
     const sendMessage = () => {
         if (inputdata.trim() !== "") {
             socket.emit("send", {
@@ -101,33 +154,45 @@ export default function ChattingRoom({ roomId }) {
             setInputData("");
         }
     };
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        let hours = date.getHours();
+        const minutes = date.getMinutes();
+        const ampm = hours >= 12 ? "오후" : "오전";
+        hours = hours % 12;
+        hours = hours ? hours : 12; // 0시를 12시로 변경
+        return `${ampm} ${hours}:${minutes < 10 ? "0" : ""}${minutes}`;
+    }
     return (
         <Maincontainer>
             <div className="chatting_container">
-                <div className="chatting_main">
-                    {msgList.map((msg) => (
-                        <div
-                            key={msg.id}
-                            className={msg.User && msg.User.id === loginUser.id ? "me" : "other"}
-                        >
-                            {msg.User
-                                ? `${msg.User.id}: ${msg.content}`
-                                : `Unknown User: ${msg.content}`}
-                        </div>
-                    ))}
-
-                    {messages.map((msg, index) => (
+                <button onClick={() => setShowChattingRoom(false)}>나가기</button>
+                <div ref={chatContainerRef} className="chatting_main">
+                    {msgList.map((msg, index) => (
                         <div
                             key={index}
-                            className={
-                                msg.type === "me"
-                                    ? "me"
-                                    : msg.type === "notice"
+                            className={`message-container ${
+                                msg.type === "notice"
                                     ? "notice"
-                                    : "other"
-                            }
+                                    : msg.User && msg.User.id !== loginUser.id
+                                    ? "received"
+                                    : ""
+                            }`}
                         >
-                            {msg.message}
+                            {msg.type !== "notice" && !msg.is_read && (
+                                <span className="read">1</span>
+                            )}
+                            <div className="message">
+                                {msg.create_at ? (
+                                    <span className="time">{formatDate(msg.create_at)}</span>
+                                ) : (
+                                    ""
+                                )}
+                                {msg.type !== "notice" &&
+                                    msg.User.id &&
+                                    msg.User.id !== loginUser.id && <span>{msg.User.id} </span>}
+                                {msg.content}
+                            </div>
                         </div>
                     ))}
                 </div>
